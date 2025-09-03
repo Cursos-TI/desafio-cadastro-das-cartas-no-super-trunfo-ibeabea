cf134ba556f22ff774c6cf2e9399b8a8d43645c2
ibeabea <bsantos.bea@gmail.com>
2025-09-03 02:49:48 +0000

    certo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,17 +2,15 @@
 
 int main (){
     char estado =  'P';
-   char codigo[20] = "PB1";
+   char carta[20] = "PB1";
    char cidade[20] = "Belém";
    int população = 1303.389;
    float area = 1.059458;
    float PIB = 21.19147;
    int pontos = 23; 
 
-
-   printf("Carta: %d\n", carta);
    printf("Estado: %c\n", estado);
-   printf("Código da Carta: %s\n", codigo);
+   printf("Código da Carta: %s\n", carta);
    printf("Nome da Cidade: %s\n", cidade);
    printf("População: %d\n", população);
    printf("Área em km: %f\n", area);
